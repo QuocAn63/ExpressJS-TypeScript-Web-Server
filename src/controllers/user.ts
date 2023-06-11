50831a9a559d1f436c957ee5879d43c34b73120f
caoquocan <caoan632002@gmail.com>
2023-06-11 16:11:54 +0700

    update user controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,12 +1,19 @@
-import { NextFunction, Response } from "express";
-import { IRequestWithOauthCallback } from "../types";
+import { NextFunction, Request, Response } from "express";
 import { IResponseData } from "../types/response";
+import userModel from "../models/user";
 
-const getUser = async (
-  req: IRequestWithOauthCallback,
+export const getAllUsers = async (
+  req: Request,
   res: Response<IResponseData>,
   next: NextFunction
 ) => {
   try {
-  } catch (err) {}
+    const queries = req.query;
+
+    const usersResponse = await userModel.find();
+
+    return res.status(200).json({ data: usersResponse });
+  } catch (err) {
+    next(err);
+  }
 };
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,14 @@
 import express, { Express, Router } from "express";
 import authenticateRoute from "./authenticate";
 import todoRoute from "./todo";
+import userRoute from "./user";
 import homeRoute from "./home";
 import { errorHandler } from "../controllers/error";
 const apiRouter: Router = express.Router();
 
 const appRouter = (app: Express) => {
   apiRouter.use("/auth", authenticateRoute);
+  apiRouter.use("/", userRoute);
 
   // api routes
   app.use("/api", apiRouter);
