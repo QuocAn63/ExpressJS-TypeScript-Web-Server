4278e1237b69c93836a6ab72d1f4a6f5a86e1056
caoquocan <caoan632002>
2023-06-24 11:12:37 +0700

    update 2

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { App } from "../src/app";
-import AuthRoute from "../src/router/auth";
-import ProductRoute from "../src/router/product";
-import PromotionRoute from "../src/router/promotion";
-import UserRoute from "../src/router/user";
+import { App } from "./app";
+import AuthRoute from "./router/auth";
+import ProductRoute from "./router/product";
+import PromotionRoute from "./router/promotion";
+import UserRoute from "./router/user";
 
 const app = new App(
   new AuthRoute(),
